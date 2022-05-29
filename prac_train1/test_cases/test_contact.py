389c370ac81fcf42047b6831aa8eb092dd8df826
xixi_tech <xujuan@ceshiren.com>
2022-05-29 17:29:47 +0800

    实战练习2 打卡功能

--- prac_train1/test_cases/test_contact.py
+++ prac_train1/test_cases/test_contact.py
@@ -8,6 +8,7 @@ from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common.exceptions import NoSuchElementException
 
+from prac_train1.base.bas_page import BasePage
 from prac_train1.utils.contact_info import ContactInfo
 from prac_train1.utils.log_util import logger
 
@@ -19,58 +20,7 @@ from prac_train1.utils.log_util import logger
 """
 
 
-class TestContact:
-    implicitly_wait_time = 30
-
-    def setup(self):
-        # 资源初始化
-        # 打开【企业微信】应用
-        caps = {}
-        caps["platformName"] = "Android"
-        # mac: adb logcat ActivityManager:I | grep "cmp"
-        # windows:adb logcat ActivityManager:I | findstr "cmp"
-        caps["appPackage"] = "com.tencent.wework"
-        caps["appActivity"] = ".launch.LaunchSplashActivity"
-        # adb devices
-        caps["deviceName"] = "emulator-5554"
-        # "True" 是绝对不可以的  要么是"true" 要么是 True
-        # 防止清缓存
-        caps["noReset"] = "true"
-        # 创建driver ,与appium server建立连接，返回一个 session
-        # driver 变成self.driver 由局部变量变成实例变量，就可以在其它的方法中引用这个实例变量了
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
-        # 隐式等待是全局的等待方式
-        self.driver.implicitly_wait(self.implicitly_wait_time)
-
-    def teardown(self):
-        self.driver.quit()
-
-    def swipe_find(self, text, num=3):
-        # 自定义滑动查找，
-        self.driver.implicitly_wait(1)
-        for i in range(num):
-            try:
-                element = self.driver.find_element(AppiumBy.XPATH, f"//*[@text='{text}']")
-                self.driver.implicitly_wait(self.implicitly_wait_time)
-                return element
-            except:
-                logger.info("未找到元素，开始滑动")
-                # 获取当前屏幕尺寸
-                # 'width', 'height'
-                size = self.driver.get_window_size()
-                width = size.get("width")
-                height = size.get("height")
-                logger.info(f"当前屏幕的宽：{width}, 高：{height}")
-                start_x = width / 2
-                start_y = height * 0.8
-                end_x = start_x
-                end_y = height * 0.3
-                duration = 2000
-                logger.info("开始滑动")
-                self.driver.swipe(start_x, start_y, end_x, end_y, duration)
-            if i == num - 1:
-                self.driver.implicitly_wait(self.implicitly_wait_time)
-                raise NoSuchElementException(f"找了{num}次 ，未找到元素{text}")
+class TestContact(BasePage):
 
     def test_addcontact(self):
         """
--- prac_train1/test_cases/test_workbench.py
+++ prac_train1/test_cases/test_workbench.py
@@ -3,33 +3,12 @@ __author__ = '霍格沃兹测试开发学社'
 __desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/topic/15860'
 """
 from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+from prac_train1.base.bas_page import BasePage
 
 
-class TestWorkbench:
-    implicitly_wait_time = 30
-
-    def setup(self):
-        # 资源初始化
-        # 打开【企业微信】应用
-        caps = {}
-        caps["platformName"] = "Android"
-        # mac: adb logcat ActivityManager:I | grep "cmp"
-        # windows:adb logcat ActivityManager:I | findstr "cmp"
-        caps["appPackage"] = "com.tencent.wework"
-        caps["appActivity"] = ".launch.LaunchSplashActivity"
-        # adb devices
-        caps["deviceName"] = "emulator-5554"
-        # "True" 是绝对不可以的  要么是"true" 要么是 True
-        # 防止清缓存
-        caps["noReset"] = "true"
-        # 创建driver ,与appium server建立连接，返回一个 session
-        # driver 变成self.driver 由局部变量变成实例变量，就可以在其它的方法中引用这个实例变量了
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
-        # 隐式等待是全局的等待方式
-        self.driver.implicitly_wait(self.implicitly_wait_time)
-
-    def teardown(self):
-        self.driver.quit()
+class TestWorkbench(BasePage):
 
     def test_daka(self):
         """
@@ -40,4 +19,9 @@ class TestWorkbench:
         验证点：提示【外出打卡成功】
         :return:
         """
-        pass
+        self.driver.find_element(AppiumBy.XPATH, "//*[@text='工作台']").click()
+        self.swipe_find("打卡").click()
+        self.driver.find_element(AppiumBy.XPATH, "//*[@text='外出打卡']").click()
+        self.driver.find_element(AppiumBy.XPATH, "//*[contains(@text, '次外出')]").click()
+        # 断言 assert  find ...最后一步，进行倒数第二步的验证
+        self.driver.find_element(AppiumBy.XPATH, "//*[@text='外出打卡成功']")
